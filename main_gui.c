a72e15753e939d18d5bebd310c6afd1a12de1db6
Alexey Ozeritskiy <aozeritsky@gmail.com>
2021-03-01 00:09:59 +0300

    Add tabs, optimizations

--- main_gui.c
+++ main_gui.c
@@ -4,16 +4,26 @@
 #include "lsystem_parser.h"
 #include "colormap_vga1.h"
 
-struct App {
+struct Tab {
+    char* name;
     cairo_surface_t *surface;
-    GtkWidget* window;
+    GtkWidget* notebook;
     GtkWidget* drawing_area;
-    GtkWidget* text_view;
+    int pageno;
     struct Line* lines;
     double min_x;
     double max_x;
     double min_y;
     double max_y;
+};
+
+struct App {
+    GtkWidget* window;
+    GtkWidget* notebook;
+    GtkWidget* text_view;
+    struct Tab** tabs;
+    int ntabs;
+    int capacity;
 };
 
 static void clear_surface(cairo_surface_t *surface)
@@ -27,39 +37,52 @@ static void clear_surface(cairo_surface_t *surface)
     cairo_destroy(cr);
 }
 
-static gboolean configure_event_cb(GtkWidget *widget, GdkEventConfigure *event, struct App* app)
+static gboolean draw_cb(GtkWidget* widget, cairo_t* cr, struct Tab* t)
 {
-    if (app->surface)
-        cairo_surface_destroy(app->surface);
+    double x0, y0;
 
-    app->surface = gdk_window_create_similar_surface(gtk_widget_get_window(widget), CAIRO_CONTENT_COLOR,
-                                                gtk_widget_get_allocated_width(widget),
-                                                gtk_widget_get_allocated_height(widget));
+    if (t->surface == NULL) {
+        return TRUE;
+    }
 
-    clear_surface(app->surface);
+    int pageno = gtk_notebook_get_current_page(GTK_NOTEBOOK(t->notebook));
+    if (pageno < 0) {
+        return TRUE;
+    }
+    if (pageno != t->pageno) {
+        return TRUE;
+    }
+
+    cairo_set_source_surface(cr, t->surface, 0, 0);
+    cairo_paint(cr);
 
     return TRUE;
 }
 
-static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, struct App*  app)
+static gboolean configure_event_cb(GtkWidget *widget, GdkEventConfigure *event, struct Tab* t)
 {
-    double x0, y0;
-
-    int w = gtk_widget_get_allocated_width (widget);
-    int h = gtk_widget_get_allocated_height (widget);
-
-    if (app->lines == NULL) {
-        return TRUE;
+    if (t->surface) {
+        cairo_surface_destroy(t->surface);
     }
+    int w = gtk_widget_get_allocated_width(widget);
+    int h = gtk_widget_get_allocated_height(widget);
 
-    double min_x = app->min_x, max_x = app->max_x, min_y = app->min_y, max_y = app->max_y;
+    t->surface = gdk_window_create_similar_surface(
+        gtk_widget_get_window(widget), CAIRO_CONTENT_COLOR,
+        w, h);
+
+    clear_surface(t->surface);
+
+    double min_x = t->min_x, max_x = t->max_x, min_y = t->min_y, max_y = t->max_y;
 
     double xx = (w - 2) / (max_x - min_x);
     double yy = (h - 2) / (max_y - min_y);
     double kk = xx < yy ? xx : yy;
 
+    cairo_t* cr = cairo_create (t->surface);
+
     int k = 0;
-    for (struct Line* it = app->lines; it != NULL; it=it->next, k++)
+    for (struct Line* it = t->lines; it != NULL; it=it->next, k++)
     {
         int x0 = (int)(1 + (it->x0 - min_x) * kk); 
         int y0 = (int)(1 + (it->y0 - min_y) * kk); y0 = h - y0 - 1;
@@ -71,14 +94,43 @@ static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, struct App*  app)
         cairo_stroke(cr);
     }
 
+    cairo_destroy (cr);
+
     return TRUE;
+}
+
+static void destroy_tabs(struct App* app) {
+    struct Line* cur = NULL;
+    struct Line* next = NULL;
+    int i;
+    for (i = 0; i < app->ntabs; i=i+1) {
+        if (app->tabs[i]->surface)
+        {
+            cairo_surface_destroy(app->tabs[i]->surface);
+            app->tabs[i]->surface = NULL;
+        }
+        if (app->notebook) {
+            gtk_widget_destroy(app->tabs[i]->drawing_area);
+        }
+        cur = app->tabs[i]->lines;
+        while (cur) {
+            next = cur->next;
+            free(cur);
+            cur = next;
+        }
+        free(app->tabs[i]->name);
+        free(app->tabs[i]);
+    }
+    app->ntabs = 0;
 }
 
 static void compile(struct App* app) {
     struct Parser* p = parser_new();
     struct Group* g = NULL;
+    struct Tab* t;
     
     GtkTextIter start, end;
+    GtkWidget* label;
     gchar* text;
     GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(app->text_view));
     gtk_text_buffer_get_start_iter(buffer, &start);
@@ -92,29 +144,50 @@ static void compile(struct App* app) {
     yylex_destroy();
 
     if (!parser_has_error(p)) {
-        struct Line* cur = NULL;
-        struct Line* next = NULL;
         struct Line* lines = NULL;
+        const char* name;
+
+        destroy_tabs(app);
+
         int l;
         char *W;
+        int pageno = 0;
         parser_print(p);
-        g = parser_group_start(p);
-        l = group_get_order(g);
-        if (l == 0) {
-            l = 2;
-        }
-        W = lsystem(g, l);
-        lines = turtle(g, W); 
-        free(W);
-        cur = app->lines;
-        while (cur) {
-            next = cur->next;
-            free(cur);
-            cur = next;
-        }
-        app->lines = lines;
-        if (lines != NULL) {
-            lines_normilize(lines, &app->min_x, &app->max_x, &app->min_y, &app->max_y);
+
+        for (g = parser_group_start(p); g; g = group_next(g), pageno = pageno+1) {
+            l = group_get_order(g);
+            if (l == 0) {
+                l = 2;
+            }
+            name = group_get_name(g);
+            W = lsystem(g, l);
+            lines = turtle(g, W); 
+            free(W);
+
+            t = calloc(1, sizeof(struct Tab));
+            t->lines = lines;
+            t->name = strdup(name);
+
+            if (lines != NULL) {
+                lines_normilize(lines, &t->min_x, &t->max_x, &t->min_y, &t->max_y);
+            }
+            t->drawing_area = gtk_drawing_area_new();
+            t->notebook = app->notebook;
+            t->pageno = pageno;
+            g_signal_connect(t->drawing_area, "configure-event", G_CALLBACK(configure_event_cb), t);
+            g_signal_connect(t->drawing_area, "draw", G_CALLBACK(draw_cb), t);
+
+            label = gtk_label_new(name);
+            gtk_notebook_append_page(GTK_NOTEBOOK(app->notebook), t->drawing_area, label);
+
+            if (app->ntabs >= app->capacity) {
+                app->capacity = (app->capacity+1)*2;
+                app->tabs = realloc(app->tabs, app->capacity*sizeof(struct Tab*));
+            }
+            app->tabs[app->ntabs++] = t;
+
+            gtk_widget_show(t->drawing_area);
+            gtk_widget_show(label);
         }
     } else {
         parser_print_error(p);
@@ -122,18 +195,6 @@ static void compile(struct App* app) {
 
     free(text);
     parser_free(p);
-}
-
-static gboolean key_press_event(GtkWidget* widget, GdkEventKey* event, struct App* app) 
-{
-    if (event->type == GDK_KEY_PRESS) {
-        if (event->keyval == GDK_KEY_F5) {
-            fprintf(stderr, "compile\n");
-            compile(app);
-            gtk_widget_queue_draw(app->drawing_area);
-        }
-    }
-    return FALSE;
 }
 
 static void open_file(char* filename, struct App* app) {
@@ -150,8 +211,10 @@ static void open_file(char* filename, struct App* app) {
 
     int size;
     while ((size = fread(buf, 1, sizeof(buf), f)) > 0) {
+        gchar* utf8 = g_utf8_make_valid(buf, size);
         gtk_text_buffer_get_end_iter(buffer, &end);
-        gtk_text_buffer_insert(buffer, &end, buf, size);
+        gtk_text_buffer_insert(buffer, &end, utf8, strlen(utf8));
+        g_free(utf8);
     }
 
     fclose(f);
@@ -189,8 +252,6 @@ open_cb (GSimpleAction *simple, GVariant      *parameter, gpointer user_data)
     }
 
     gtk_widget_destroy (dialog);
-
-    fprintf(stderr, "open\n");
 }
 
 static void
@@ -204,7 +265,6 @@ run_cb (GSimpleAction *simple, GVariant      *parameter, gpointer user_data)
 {
     struct App* app = (struct App*)user_data;
     compile(app);
-    gtk_widget_queue_draw(app->drawing_area);
 }
 
 static GtkWidget* create_toolbar(struct App* app) {
@@ -238,8 +298,9 @@ static void on_app_activate(GApplication *a, struct App* app) {
     GtkWidget* window;
     GtkWidget* box; // drawing_area and text
     GtkWidget* main_box; // toolbar and other
-    GtkWidget* drawing_area;
     GtkWidget* text_view;
+    GtkWidget* notebook;
+    GtkWidget* scrolled;
     GtkTextBuffer *buffer;
     GActionEntry action_entries[] =
     {
@@ -265,22 +326,25 @@ static void on_app_activate(GApplication *a, struct App* app) {
 
     main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    drawing_area = gtk_drawing_area_new();
+    scrolled = gtk_scrolled_window_new(NULL, NULL);
     text_view = gtk_text_view_new();
+    notebook = gtk_notebook_new();
+    gtk_notebook_set_scrollable(GTK_NOTEBOOK(notebook), TRUE);
     buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (text_view));
     gtk_text_buffer_set_text (buffer, text, -1);
     gtk_text_view_set_monospace(GTK_TEXT_VIEW(text_view), TRUE);
 
     gtk_container_add(GTK_CONTAINER(window), main_box);
+    gtk_container_add(GTK_CONTAINER(scrolled), text_view);
 
     gtk_box_pack_start(GTK_BOX(main_box), create_toolbar(app), FALSE, TRUE, 0);
     gtk_box_pack_end(GTK_BOX(main_box), box, TRUE, TRUE, 0);
 
-    gtk_box_pack_start(GTK_BOX(box), drawing_area, TRUE, TRUE, 0);
-    gtk_box_pack_end(GTK_BOX(box), text_view, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), notebook, TRUE, TRUE, 0);
+    gtk_box_pack_end(GTK_BOX(box), scrolled, TRUE, TRUE, 0);
     gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
 
-    app->drawing_area = drawing_area;
+    app->notebook = notebook;
     app->text_view = text_view;
     app->window = window;
 
@@ -290,10 +354,6 @@ static void on_app_activate(GApplication *a, struct App* app) {
 
     const char *accels[] = {"F5", NULL};
     gtk_application_set_accels_for_action(GTK_APPLICATION(a), "lsystem.run", accels);
-
-    g_signal_connect(drawing_area, "configure-event", G_CALLBACK(configure_event_cb), app);
-    g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_cb), app);
-    g_signal_connect(window, "key-press-event", G_CALLBACK(key_press_event), app);
 };
 
 int main(int argc, char** argv) {
@@ -312,10 +372,10 @@ int main(int argc, char** argv) {
     int status = g_application_run(G_APPLICATION(a), argc, argv);
     g_object_unref(a);
 
-    if (app.surface)
-    {
-        cairo_surface_destroy(app.surface);
-    }
+    fprintf(stderr, "done\n");
+    app.notebook = NULL;
+    destroy_tabs(&app);
+    free(app.tabs);
 
     return status;
 }
